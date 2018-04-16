9b5f17bb1aeb815d9814a08d678d875be632fe5a
Alex Freeman <info@whist.org>
2018-04-16 14:59:17 +0800

    命令行界面传入网址，程序可以分析出文章的标题、关键词、来源时间、文章正文等，可以下载文章的图片并在图片的原位置记录

--- huanqiu_spider.py
+++ huanqiu_spider.py
@@ -1,45 +1,54 @@
 # coding: utf-8
 import requests
 import sys
+
+import os
 from bs4 import BeautifulSoup as bs
 
+IMG_DIR = "./img/"
+IMG_URL = "img/%s"
+data = {}
+
+if not os.path.exists(IMG_DIR):
+    os.mkdir(IMG_DIR)
+
 def get_content(url):
-    data = {}
     r = requests.get(url)
     r.encoding = 'utf-8'
     soup = bs(r.text,'lxml')
-    title = soup.title.string.strip(' _军事_环球网') # title
-    kw = soup.find_all('meta')[2]['content']  # keywords
-    des = soup.find_all('meta')[3]['content']  # description
-    source = soup.select('.l_a > .la_tool > .la_t_b > a')[0].string
-    time = soup.select('.l_a > .la_tool > .la_t_a')[0].string
-    data = {
-        'url':url,
-        'title':title,
-        'keywords':kw,
-        'description':des,
-        'source':source,
-        'time':time
-    }
-    print(data)
-    paras = soup.select('.l_a > .la_con')[0].find_all('p')
-    fo = open('news.txt',"w+")
-    for para in paras:
-        if len(para) > 0 and para.string != None:
-            fo.writelines(para.string.strip() + "\n")
-    fo.close()
-    
-    try:
-        img_url = soup.select('.l_a > .la_con > p > img')[0]['src']
-        img_res = requests.get(img_url,stream=True)
-        img_name = "news.jpg"
-        with open(img_name,"wb") as f:
-            for chunk in img_res:
-                f.write(chunk)
-    except Exception:
-        print("This article doesn't have pictures.")
-        # print(Exception) <class 'Exception'>
+    title = soup.select('.l_a > h1')[0].string# title
+    keywords = soup.find('meta',{'name':'keywords'})['content']# keywords
+    des = soup.find('meta',{'name':'description'})['content']
+    source = soup.select('.l_a > div.la_tool > span.la_t_b')[0].string.strip()
+    time = soup.select('.l_a > div.la_tool > span.la_t_a')[0].string
         
+    body_content = soup.find('div', {'class': 'la_con'})
+    p_list = []
+    for i, p in enumerate(body_content.find_all('p')):
+        if p.attrs is not None:
+            del p.attrs
+
+        img_lists = p.find_all('img')
+        for img in img_lists:
+            img_url = img["src"]
+            img_name = img_url.split("/")[-1]
+            nr = requests.get(img_url, stream=True)
+            with open(os.path.join(IMG_DIR, img_name), "wb") as f:
+                for chunk in nr.iter_content(chunk_size=512):
+                    f.write(chunk)
+            img["src"] = IMG_URL % img_name
+        p_list.append(str(p))
+    content = "\n".join(p_list)  
+    
+    ##save in a text file
+    news_name = "%s.txt" % title[:5]
+    with open(news_name,"w+",encoding='utf-8') as f:
+        f.write(title + '\n')
+        f.write(keywords + '\n')
+        f.write(des + '\n')
+        f.write(source + '\n')
+        f.write(time + '\n')
+        f.write(content)
+    
 if __name__ == "__main__":
-    # print(type(sys.argv[1:]))   # It's a list
-    get_content(sys.argv[1:][0])
+    get_content(sys.argv[1])
